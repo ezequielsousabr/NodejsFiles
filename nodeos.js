7653673303ec45c9fe571e4c75aa127ed844e7c8
Ezequiel L Sousa <ezequiel.sousa@yahoo.com>
2023-03-23 16:35:29 -0300

    Adicionando informações de sistema...

--- nodeos.js
+++ nodeos.js
@@ -1,4 +1,5 @@
 /*
 Obter informações da interface de rede com Nodejs */
 const os = require('node:os')
-console.log("Informações da interface de rede: ", os.networkInterfaces())
+console.log("Informações de Sistema: ", os.hostname(), os.machine());
+console.log("Informações da interface de rede: ", os.networkInterfaces());
