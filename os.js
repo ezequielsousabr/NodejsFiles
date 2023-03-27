cfba37bce3bf757dec3f2341e2aa35cc40db33f7
Ezequiel L Sousa <ezequiel.sousa@yahoo.com>
2023-03-27 01:20:36 -0300

    Informações de rede com nodejs

--- os.js
+++ os.js
@@ -1,6 +1,4 @@
 /*
 Obter informações da interface de rede com Nodejs */
 const os = require('node:os')
-console.log("Informações de usuário: ", os.userInfo());
-console.log("Informações de Sistema: ", os.hostname(), os.machine());
-console.log("Informações da interface de rede: ", os.networkInterfaces());
+console.log(os.networkInterfaces());
